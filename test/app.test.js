5cb76fefb07e19f0117bc2ce71c66fdb881fbfcb
YETUKURI SAI JAGAN <nani.y9966@gmail.com>
2025-03-12 16:23:35 +0530

    v-1

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('Basic Transport App Tests', function() {
+  it('should return 1 when 1 is returned', function() {
+    assert.equal(1, 1);
+  });
+
+  // Add more tests here to test your actual app logic
+
+});
